7bf7c9e1d4b9a8df1d11d3192dfc3468409742a6
Weiberle17 <maxweiberle63@gmail.com>
2023-07-20 14:58:19 +0200

    style: Formatting

--- test.py
+++ test.py
@@ -8,7 +8,6 @@ from pdfminer.layout import LAParams
 
 output_string = StringIO()
 with open('../PA2/Formulare/Elternzeit Antrag Beschäftigte - Max Mustermann.pdf', 'rb') as fin:
-    extract_text_to_fp(fin, output_string, laparams=LAParams(),
-                       output_type='xml', codec=None)
+    extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='xml', codec=None)
 
 print(output_string.getvalue())
