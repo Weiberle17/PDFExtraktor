3c2874d68377cf7356e00a18b83904bec99764a4
Weiberle17 <maxweiberle63@gmail.com>
2023-08-29 20:18:17 +0200

    feat: Splitting textlines to get words

--- pdf_extraktor.py
+++ pdf_extraktor.py
@@ -1,13 +1,29 @@
 #!./venv/bin/python3
 
-from io import StringIO
-
+import sys
+import argparse
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 
-output_string = StringIO()
-with open('../PA2/Formulare/Elternzeit Antrag Beschäftigte - Max Mustermann.pdf', 'rb') as fin:
-    extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='xml', codec=None)
+# Argument parsing for cli tool
+parser = argparse.ArgumentParser(prog='pdf_extraktor', description='Extrahieren von Daten aus PDF-Dokumenten in parsen in xml, oder json')
+parser.add_argument('Eingabedatei')
+parser.add_argument('Ausgabeformat', choices=['xml', 'json'])
+args = parser.parse_args()
 
-print(output_string.getvalue())
+# Delimiter
+dl = "<textline"
+
+# Using pdfminer to get xml template to work with
+output_string = StringIO()
+with open(args.Eingabedatei, 'rb') as fin:
+  extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='xml', codec=None)
+
+# Parsing string into Sections to turn letters into words
+parsed_string = [dl+e for e in output_string.getvalue().split(dl) if e]
+
+for section in parsed_string:
+  print("Section:")
+  print(section)
+  print()
