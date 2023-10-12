f7e05cb004cb50c9b8cf6467225c4e078cb66cd6
Weiberle17 <maxweiberle63@gmail.com>
2023-10-12 16:27:01 +0200

    refactor: Modularisierung

--- pdf_extraktor.py
+++ pdf_extraktor.py
@@ -1,43 +1,19 @@
 #!./venv/bin/python3
 
-import argparse
-import xml.etree.ElementTree as ET
-from xml.dom import minidom
+import input_handler
+import doc_recognizer
+
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 
-# Argument parsing for cli tool
-parser = argparse.ArgumentParser(prog='pdf_extraktor', description='Extrahieren von Daten aus PDF-Dokumenten in parsen in xml, oder json')
-parser.add_argument('Eingabedatei')
-parser.add_argument('Ausgabeformat', choices=['xml', 'json'])
-args = parser.parse_args()
+doc_type = doc_recognizer.recognize_doc(input_handler.args.Eingabedatei)
 
-# Using pdfminer to get xml template to work with
-output_string = StringIO()
-with open(args.Eingabedatei, 'rb') as fin:
-  extract_text_to_fp(fin, output_string, laparams=LAParams(), output_type='xml', codec=None)
+AusgabeDatei = str(input_handler.args.Eingabedatei).split('/')[-1].split('.')[0] + '.xml'
 
-AusgabeDatei = str(args.Eingabedatei).split('/')[-1].split('.')[0] + '.xml'
-
-def prettify(elem):
-  rough_string = ET.tostring(elem, 'utf-8')
-  reparsed = minidom.parseString(rough_string)
-  return reparsed.toprettyxml(indent="  ")
-
-# Remove first line to allow ET.parse to work properly
-lines = output_string.getvalue().split('\n')
-tree_string = StringIO()
-tree_string.writelines(''.join(lines[1:]))
-tree_string.seek(0)
-
-tree = ET.parse(tree_string)
-root = tree.getroot()
-
-# Einzelne Textstellen können so angesprochen und verändert werden
-root[0][0][0].text = 'hallo'
-print(root[0][0][0].text)
-
-f = open(AusgabeDatei, "w")
-f.write(prettify(root))
-f.close()
+# # Einzelne Textstellen können so angesprochen und verändert werden
+# print(root[0][0][0].text)
+#
+# f = open(AusgabeDatei, "w")
+# f.write(prettify(root))
+# f.close()
