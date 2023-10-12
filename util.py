1c1970511016f4a5f5e5f44ee8b2d691f098d3e6
Weiberle17 <maxweiberle63@gmail.com>
2023-10-12 16:29:20 +0200

    feat: Added function to parse indexes

--- util.py
+++ util.py
@@ -1,6 +1,10 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
+def getIndexe(input_string):
+  indexe = [int(index) for index in str(input_string) if index.isdigit()]
+  return indexe
+
 def prettify(elem):
   rough_string = ET.tostring(elem, 'utf-8')
   reparsed = minidom.parseString(rough_string)
