fd1a2ba5fbda4e8402a686446e2697632a2cccda
Weiberle17 <maxweiberle63@gmail.com>
2023-10-13 12:10:48 +0200

    WIP: feat: Proper arguments

--- debugger.py
+++ debugger.py
@@ -1,18 +1,38 @@
 #!./venv/bin/python3
 
-import input_handler
 import tree_builder
+import doc_recognizer
+import anonymizer
+import util
 
+import argparse
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
 
-def prettify(elem):
-  rough_string = ET.tostring(elem, 'utf-8')
-  reparsed = minidom.parseString(rough_string)
-  return reparsed.toprettyxml(indent="  ")
+# TODO: Add more options later on
+parser = argparse.ArgumentParser(prog='pdf_extraktor', description='Debug module for pdf_extraktor')
+parser.add_argument('Eingabedatei')
+parser.add_argument('-dt', '--doc_type', action='store_true', help='Display recognized doc type')
+parser.add_argument('-re', '--raw_extraction', action='store_true', help='Get raw extraction - written to debug/ausgabe.xml')
+parser.add_argument('-a', '--anonymize', action='store_true', help='Get anonymized data - written to debug/ausgabe.xml')
+args = parser.parse_args()
 
-tree = tree_builder.build_tree(input_handler.args.Eingabedatei)
+if args.doc_type:
+  tree = tree_builder.build_tree(args.Eingabedatei)
+  print(doc_recognizer.recognize_doc(tree))
 
-f = open("debug/ausgabe.xml", "w")
-f.write(prettify(tree))
-f.close()
+if args.raw_extraction:
+  tree = tree_builder.build_tree(args.Eingabedatei)
+  f = open("debug/ausgabe.xml", "w")
+  f.write(util.prettify(tree))
+  print("Written to debug/ausgabe.xml")
+  f.close()
+
+if args.anonymize:
+  tree = tree_builder.build_tree(args.Eingabedatei)
+  doc_type = doc_recognizer.recognize_doc(tree)
+  anonymized = anonymizer.anonymize(doc_type, tree)
+  f = open("debug/ausgabe.xml", "w")
+  f.write(anonymized)
+  print("Written to debug/ausgabe.xml")
+  f.close()
