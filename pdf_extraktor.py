bf8cfddfb90859a16822c2769bf1a5c3aa31915a
Weiberle17 <maxweiberle63@gmail.com>
2023-10-13 12:10:13 +0200

    refactor: Modularisierung

--- pdf_extraktor.py
+++ pdf_extraktor.py
@@ -1,15 +1,19 @@
 #!./venv/bin/python3
 
 import input_handler
+import tree_builder
 import doc_recognizer
+import anonymizer
+import output_handler
 
 from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 
-doc_type = doc_recognizer.recognize_doc(input_handler.args.Eingabedatei)
-
-AusgabeDatei = str(input_handler.args.Eingabedatei).split('/')[-1].split('.')[0] + '.xml'
+input_tree = tree_builder.build_tree(input_handler.args.Eingabedatei)
+doc_type = doc_recognizer.recognize_doc(input_tree)
+anonymized_tree = anonymizer.anonymize(doc_type, input_tree)
+output_handler.output(input_handler.args.Eingabedatei, anonymized_tree)
 
 # # Einzelne Textstellen können so angesprochen und verändert werden
 # print(root[0][0][0].text)
