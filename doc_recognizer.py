4bf5cdc60064e44da905c3f89972a8dfc8ce2934
Weiberle17 <maxweiberle63@gmail.com>
2023-10-13 12:09:52 +0200

    refactor: Change input

--- doc_recognizer.py
+++ doc_recognizer.py
@@ -6,14 +6,13 @@ from io import StringIO
 from pdfminer.high_level import extract_text_to_fp
 from pdfminer.layout import LAParams
 
-def recognize_doc(input_file):
-  tree = tree_builder.build_tree(input_file)
+def recognize_doc(input_tree):
   # Check for Formulare
-  if 'Antrag' in str(tree[0][0][0].text):
+  if 'Antrag' in str(input_tree[0][0][0].text):
     file_types = ET.parse('doc_config/file_types.xml').getroot()
     for formular in file_types[0]:
       index0, index1, index2 = util.getIndexe(formular.get('index'))
-      if str(formular.text) in str(tree[index0][index1][index2].text):
+      if str(formular.text) in str(input_tree[index0][index1][index2].text):
         return formular.tag
     else:
       return "Keine Konfiguration für dieses Formular gefunden"
