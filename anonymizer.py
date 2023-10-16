5f5e0cd0b1e49701a70d056b15f7ee7593040816
Weiberle17 <maxweiberle63@gmail.com>
2023-10-16 19:45:16 +0200

    fix: Custom exception for wrong format

--- anonymizer.py
+++ anonymizer.py
@@ -1,4 +1,5 @@
 import util
+from util import FormException
 
 import xml.etree.ElementTree as ET
 
--- pdf_extraktor.py
+++ pdf_extraktor.py
@@ -5,7 +5,7 @@ import tree_builder
 import doc_recognizer
 import anonymizer
 import output_handler
-from util import MissingConfigException, DocumentTypeRecognitionException
+from util import FormException, MissingConfigException, DocumentTypeRecognitionException
 import sys
 
 try:
@@ -13,6 +13,6 @@ try:
   doc_type = doc_recognizer.recognize_doc(input_tree)
   anonymized_tree = anonymizer.anonymize(doc_type, input_tree)
   output_handler.output(input_handler.args.Eingabedatei, anonymized_tree, input_handler.args.Ausgabeformat)
-except (MissingConfigException, DocumentTypeRecognitionException) as e:
+except (MissingConfigException, DocumentTypeRecognitionException, FormException) as e:
   print(f"Error: {e.message}")
   sys.exit(1)
