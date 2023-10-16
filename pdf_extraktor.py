e81ba3d5c01b52c6d43ecfbb8a49422ff869d23f
Weiberle17 <maxweiberle63@gmail.com>
2023-10-16 19:38:58 +0200

    fix: Try-except block for good error messages

--- pdf_extraktor.py
+++ pdf_extraktor.py
@@ -5,19 +5,14 @@ import tree_builder
 import doc_recognizer
 import anonymizer
 import output_handler
+from util import MissingConfigException, DocumentTypeRecognitionException
+import sys
 
-from io import StringIO
-from pdfminer.high_level import extract_text_to_fp
-from pdfminer.layout import LAParams
-
-input_tree = tree_builder.build_tree(input_handler.args.Eingabedatei)
-doc_type = doc_recognizer.recognize_doc(input_tree)
-anonymized_tree = anonymizer.anonymize(doc_type, input_tree)
-output_handler.output(input_handler.args.Eingabedatei, anonymized_tree)
-
-# # Einzelne Textstellen können so angesprochen und verändert werden
-# print(root[0][0][0].text)
-#
-# f = open(AusgabeDatei, "w")
-# f.write(prettify(root))
-# f.close()
+try:
+  input_tree = tree_builder.build_tree(input_handler.args.Eingabedatei)
+  doc_type = doc_recognizer.recognize_doc(input_tree)
+  anonymized_tree = anonymizer.anonymize(doc_type, input_tree)
+  output_handler.output(input_handler.args.Eingabedatei, anonymized_tree, input_handler.args.Ausgabeformat)
+except (MissingConfigException, DocumentTypeRecognitionException) as e:
+  print(f"Error: {e.message}")
+  sys.exit(1)
