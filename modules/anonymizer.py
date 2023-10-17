4eb58592e3b0d7c5a58c1789524dc8d38364222e
Weiberle17 <maxweiberle63@gmail.com>
2023-10-17 17:41:25 +0200

    docs: TODOs

--- modules/anonymizer.py
+++ modules/anonymizer.py
@@ -2,6 +2,8 @@ from modules import util, exceptions
 
 import xml.etree.ElementTree as ET
 
+# TODO: Exceptions for files
+# -config_file and anonymize_file
 def anonymize(file_type, input_tree):
   config_file = ET.parse("doc_config/file_types/" + file_type + ".xml").getroot()
   anonymize_file = ET.parse("doc_config/anonymize_mustermensch.xml").getroot()
