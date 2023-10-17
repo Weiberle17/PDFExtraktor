782a71c50fb9055f897225268e9dd54336a6ff9a
Weiberle17 <maxweiberle63@gmail.com>
2023-10-17 15:03:04 +0200

    fix: Use utf8 for json

--- output_handler.py
+++ output_handler.py
@@ -12,7 +12,7 @@ def output(file_name, input_tree, output_type):
     output = input_tree
   elif output_type == "json":
     AusgabeDatei = "output/" + file_name.split(os.sep)[-1].split('.')[0] + '.json'
-    output = json.dumps(xmltodict.parse(input_tree), indent=2)
+    output = json.dumps(xmltodict.parse(input_tree), indent=2, ensure_ascii=False)
   f = open(AusgabeDatei, "w", encoding="utf-8")
   f.write(str(output))
   f.close()
