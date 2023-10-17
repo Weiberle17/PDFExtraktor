22642d41e60d25b42b25419bf0d6a757c62769b8
Weiberle17 <maxweiberle63@gmail.com>
2023-10-17 14:55:30 +0200

    fix: Work with windows file paths

--- output_handler.py
+++ output_handler.py
@@ -2,15 +2,16 @@
 
 import json
 import xmltodict
+import os
 
 def output(file_name, input_tree, output_type):
   AusgabeDatei = ""
   output = ""
   if output_type == "xml":
-    AusgabeDatei = "output/" + file_name.split('/')[-1].split('.')[0] + '.xml'
+    AusgabeDatei = "output/" + file_name.split(os.sep)[-1].split('.')[0] + '.xml'
     output = input_tree
   elif output_type == "json":
-    AusgabeDatei = "output/" + file_name.split('/')[-1].split('.')[0] + '.json'
+    AusgabeDatei = "output/" + file_name.split(os.sep)[-1].split('.')[0] + '.json'
     output = json.dumps(xmltodict.parse(input_tree), indent=2)
   f = open(AusgabeDatei, "w")
   f.write(str(output))
