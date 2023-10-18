cbc573c441ce594299abae236915a7769aa54fc0
Weiberle17 <maxweiberle63@gmail.com>
2023-10-18 10:55:26 +0200

    feat: Added textbox counter to identify wrong file formats

--- modules/doc_recognizer.py
+++ modules/doc_recognizer.py
@@ -9,11 +9,11 @@ def recognize_doc(input_tree):
     for formular in file_types[0]:
       index0, index1, index2 = util.getIndexe(formular.get('index'))
       if str(formular.text) in str(input_tree[index0][index1][index2].text):
-        return formular.tag
+        return (formular.tag, formular.get('textboxes'))
     raise exceptions.MissingConfigException()
   else:
     for handbuch in file_types[1]:
       index0, index1, index2 = util.getIndexe(handbuch.get('index'))
       if str(handbuch.text) in str(input_tree[index0][index1][index2].text):
-        return handbuch.tag
+        return (handbuch.tag, handbuch.get('textboxes'))
     raise exceptions.DocumentTypeRecognitionException()
