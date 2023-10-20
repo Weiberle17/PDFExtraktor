18a96d8d376bfbcb832d6bf5032404703ca5644a
Weiberle17 <maxweiberle63@gmail.com>
2023-10-20 07:55:48 +0200

    fix: Help message

--- modules/input_handler.py
+++ modules/input_handler.py
@@ -1,7 +1,7 @@
 import argparse
 
 # Argument parsing for cli tool
-parser = argparse.ArgumentParser(prog='pdf_extraktor', description='Extrahieren von Daten aus PDF-Dokumenten in parsen in xml, oder json')
+parser = argparse.ArgumentParser(prog='pdf_extraktor', description='Extrahieren und anonymisieren von Daten aus PDF-Dokumenten. Ausgabe in xml oder json')
 parser.add_argument('Eingabedatei')
 parser.add_argument('Ausgabeformat', choices=['xml', 'json'])
 args = parser.parse_args()
